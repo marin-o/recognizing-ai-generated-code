867c3f70d4958cb8b754fe04a9b5107e913ae814
Marino J <marinoj.business@gmail.com>
2025-06-03 16:48:23 +0200

    feat: CST feature extraction notebook

--- src/experiments/baseline.py
+++ src/experiments/baseline.py
@@ -4,6 +4,7 @@ import sys
 import os
 
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+
 import torch
 from torch.utils.data import DataLoader
 from torch.optim.lr_scheduler import StepLR
